aaf62d1054f4f8b7bc3b8bf71407e58dc1d03cf2
Ruslan Piskarov <ruslan.piskarev@gmail.com>
2017-02-07 08:54:54 +0200

    if this file XML2ZohoContactList.php is useful for your soft, I will sent as json instead of html -> that's a fine idea

--- XML2ZohoContactList.php
+++ XML2ZohoContactList.php
@@ -7,32 +7,6 @@ use Helpers\Config;
 $config = new Config;
 $config->load('./config/config.php');
 
-function build_table($array) {
-  // Start table.
-  $html = '<table>';
-  // Header row.
-  $html .= '<tr>';
-  foreach ($array[0] as $key => $value) {
-    $html .= '<th>' . $key . '</th>';
-  }
-  $html .= '</tr>';
-
-  // Data rows.
-  foreach ($array as $key => $value) {
-    $html .= '<tr>';
-    foreach ($value as $key2 => $value2) {
-      $html .= '<td>' . $value2 . '</td>';
-    }
-    $html .= '</tr>';
-  }
-
-  // Finish table.
-  $html .= '</table>';
-
-  return $html;
-}
-
-
 $zoho = new ZohoBooksApi(
   $config->get('zoho.authtoken'),
   $config->get('zoho.organizationID')
@@ -40,18 +14,4 @@ $zoho = new ZohoBooksApi(
 
 $contacts = $zoho->ContactsListAll();
 
-print build_table($contacts);
-
-print "<style type='text/css'>
-table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-}
-
-td, th {
-    border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px;
-}
-</style>";
+print json_encode($contacts);
