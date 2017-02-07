862d8602596f74372f8eff4c357c394be8aab29a
Ruslan Piskarov <ruslan.piskarev@gmail.com>
2017-02-07 09:36:44 +0200

    Fixes typo in URL

--- example-sent-files-to-app.php
+++ example-sent-files-to-app.php
@@ -13,7 +13,7 @@
  * Return json result.
  */
 
-$target_url = 'http://http://api.jaleatech.com//XML2ZohoScript.php?appAuthToken=89759375937693758292';
+$target_url = 'http://http://api.jaleatech.com/XML2ZohoScript.php?appAuthToken=89759375937693758292';
 
 // This needs to be the full path to the file you want to send.
 $xml_file_name_with_full_path = realpath('./JobFinal-testing-payment-example-zero.xml');
